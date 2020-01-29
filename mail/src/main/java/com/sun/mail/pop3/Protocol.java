8ac4229d3fac640bacc987e64ff7cf5e99352f1d
Bill Shannon <bill.shannon@oracle.com>
2020-01-29 14:50:20 -0800

    Avoid NPE in POP3Store.close - fix #413

--- mail/src/main/java/com/sun/mail/pop3/Protocol.java
+++ mail/src/main/java/com/sun/mail/pop3/Protocol.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -355,7 +355,8 @@ class Protocol {
      */
     void close() {
 	try {
-	    socket.close();
+	    if (socket != null)
+		socket.close();
 	} catch (IOException ex) {
 	    // ignore it
 	} finally {
