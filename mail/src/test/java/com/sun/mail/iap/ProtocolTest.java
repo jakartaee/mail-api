3d2217d5e9b8091d36cc512d11b2e18aa3438142
jmehrens <jason_mehrens@hotmail.com>
2020-04-23 21:53:54 -0500

    Android SocketChannel workaround not working around #403

--- mail/src/test/java/com/sun/mail/iap/ProtocolTest.java
+++ mail/src/test/java/com/sun/mail/iap/ProtocolTest.java
@@ -17,12 +17,22 @@
 package com.sun.mail.iap;
 
 import java.io.*;
+import java.lang.reflect.Method;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
 import java.util.Properties;
+import java.util.Set;
 
 import com.sun.mail.test.NullOutputStream;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test the Protocol class.
@@ -81,5 +91,245 @@ public final class ProtocolTest {
 	p = new Protocol(nullis, nullps, props, false);
 	tag = p.writeCommand("CMD", null);
 	assertEquals("A0", tag);
+    }
+
+    @Test
+    public void testLayer1Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer1of5()) {
+                assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testLayer2Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer2of5()) {
+                assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testLayer3Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer3of5()) {
+                assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testLayer4Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer4of5()) {
+            assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testLayer5Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer5of5()) {
+            assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testRenamed1Socket() throws IOException, ProtocolException {
+        try (RenamedAbstractSocket s = new RenamedSocketLayer1of3()) {
+            assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testRenamed2Socket() throws IOException, ProtocolException {
+        try (RenamedAbstractSocket s = new RenamedSocketLayer2of3()) {
+                assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testRenamed3Socket() throws IOException, ProtocolException {
+        try (RenamedAbstractSocket s = new RenamedSocketLayer3of3()) {
+            assertTrue(findSocketChannel(s) instanceof FakeSocketChannel);
+        }
+    }
+    
+    @Test
+    public void testHidden1Socket() throws IOException, ProtocolException {
+        try (HiddenAbstractSocket s = new HiddenSocket1of2()) {
+            //This could be implemented to find the socket. 
+            //However, we would have fetch field value to inspect the object.
+            //Most reads will not be what we are looking for so best to give up.
+            //Feel free to change the policy later if needed.
+            assertNull(findSocketChannel(s));
+        }
+    }
+    
+    @Test
+    public void testHidden2Socket() throws IOException, ProtocolException {
+        try (HiddenAbstractSocket s = new HiddenSocket2of2()) {
+            //This could be implemented to find the socket. 
+            //However, we would have fetch field value to inspect the object.
+            //Most reads will not be what we are looking for so best to give up.
+            //Feel free to change the policy later if needed.
+            assertNull(findSocketChannel(s));
+        }
+    }
+    
+    private SocketChannel findSocketChannel(Socket s) throws IOException {
+        try {
+	    Method m = Protocol.class.getDeclaredMethod("findSocketChannel", Socket.class);
+	    m.setAccessible(true);
+	    return (SocketChannel) m.invoke((Object) null, s);
+	} catch (RuntimeException re) {
+	    throw re;
+	} catch (Exception e) {
+	    throw new IOException(e);
+	}
+    }
+    
+    private static class RenamedSocketLayer3of3 extends RenamedSocketLayer1of3 {
+    }
+    
+    private static class RenamedSocketLayer2of3 extends RenamedSocketLayer1of3 {
+    }
+    
+    private static class RenamedSocketLayer1of3 extends RenamedAbstractSocket {    
+    }
+    
+    private static abstract class RenamedAbstractSocket extends Socket {
+        @SuppressWarnings("unused") //Accessed via reflection.
+        private Socket tekcos = new WrappedSocket();
+    }
+    
+    private static class Layer5of5 extends Layer4of5 {
+    }
+    private static class Layer4of5 extends Layer3of5 {
+    }
+    private static class Layer3of5 extends Layer2of5 {
+    }
+    private static class Layer2of5 extends Layer1of5 {
+    }
+    private static class Layer1of5 extends LayerAbstractSocket {
+    }
+    
+    private static abstract class LayerAbstractSocket extends Socket {
+        @SuppressWarnings("unused") //Accessed via reflection.
+        private final Socket socket = new WrappedSocket();
+    }
+    
+    private static class WrappedSocket extends Socket {
+        @Override
+        public SocketChannel getChannel() {
+            return new FakeSocketChannel();
+        }
+    }
+    
+    private static class HiddenSocket2of2 extends HiddenSocket1of2 {
+    }
+    
+    private static class HiddenSocket1of2 extends HiddenAbstractSocket {
+    }
+    
+    private static abstract class HiddenAbstractSocket extends Socket {
+        @SuppressWarnings("unused") //Need to be present in case of access via reflection.
+        private final Object hidden = new WrappedSocket();
+    }
+    
+    private static class FakeSocketChannel extends SocketChannel {
+        
+        FakeSocketChannel() {
+            super((SelectorProvider) null);
+        }
+
+        @Override
+        public SocketChannel bind(SocketAddress local) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> SocketChannel setOption(SocketOption<T> name, T value) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public SocketChannel shutdownInput() throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public SocketChannel shutdownOutput() throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Socket socket() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isConnected() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isConnectionPending() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean connect(SocketAddress remote) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean finishConnect() throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public SocketAddress getRemoteAddress() throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int read(ByteBuffer dst) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public long read(ByteBuffer[] dsts, int offset, int length) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int write(ByteBuffer src) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public long write(ByteBuffer[] srcs, int offset, int length) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public SocketAddress getLocalAddress() throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        protected void implCloseSelectableChannel() throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        protected void implConfigureBlocking(boolean block) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> T getOption(SocketOption<T> name) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Set<SocketOption<?>> supportedOptions() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
