8d30252ec3b198f74986291da051ab44fb910965
Lukas Jungmann <lukas.jungmann@oracle.com>
2020-06-03 12:25:22 +0200

    Switch demos over to jakarta servlets
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- webapp/src/main/java/demo/FilterServlet.java
+++ webapp/src/main/java/demo/FilterServlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -11,8 +11,8 @@
 package demo;
 
 import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 
 /**
  * This servlet is used to determine whether the user is logged in before
