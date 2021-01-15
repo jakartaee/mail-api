515c22ade843e630e0d7c2d38b6933dc6d696d44
jmehrens <jason_mehrens@hotmail.com>
2021-01-14 20:43:15 -0600

    Android SocketChannel workaround not working around #403 (#441)
    (cherry picked from commit ba9bd7ecf3a19bc38731b33a888fef2c574a1b80)

--- mail/src/test/java/com/sun/mail/iap/ProtocolTest.java
+++ mail/src/test/java/com/sun/mail/iap/ProtocolTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -17,12 +17,17 @@
 package com.sun.mail.iap;
 
 import java.io.*;
+import java.lang.reflect.Method;
+import java.net.Socket;
+import java.nio.channels.SocketChannel;
 import java.util.Properties;
 
 import com.sun.mail.test.NullOutputStream;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test the Protocol class.
@@ -81,5 +86,202 @@ public final class ProtocolTest {
 	p = new Protocol(nullis, nullps, props, false);
 	tag = p.writeCommand("CMD", null);
 	assertEquals("A0", tag);
+    }
+
+    @Test
+    public void testLayer1Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer1of5()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testLayer2Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer2of5()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testLayer3Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer3of5()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testLayer4Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer4of5()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testLayer5Socket() throws IOException, ProtocolException {
+        try (LayerAbstractSocket s = new Layer5of5()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    
+    @Test
+    public void testRenamed1Socket() throws IOException, ProtocolException {
+        try (RenamedAbstractSocket s = new RenamedSocketLayer1of3()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testRenamed2Socket() throws IOException, ProtocolException {
+        try (RenamedAbstractSocket s = new RenamedSocketLayer2of3()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testRenamed3Socket() throws IOException, ProtocolException {
+        try (RenamedAbstractSocket s = new RenamedSocketLayer3of3()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testNullSocketsRenamed() throws IOException, ProtocolException {
+        try (RenamedAbstractSocket s = new NullSocketsRenamedSocket()) {
+            findSocketChannel(s);
+            assertTrue(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testHidden1Socket() throws IOException, ProtocolException {
+        try (HiddenAbstractSocket s = new HiddenSocket1of2()) {
+            //This could be implemented to find the socket. 
+            //However, we would have fetch field value to inspect the object.
+            //Most reads will not be what we are looking for so best to give up.
+            //Feel free to change the policy later if needed.
+            findSocketChannel(s);
+            assertFalse(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testHidden2Socket() throws IOException, ProtocolException {
+        try (HiddenAbstractSocket s = new HiddenSocket2of2()) {
+            //This could be implemented to find the socket. 
+            //However, we would have fetch field value to inspect the object.
+            //Most reads will not be what we are looking for so best to give up.
+            //Feel free to change the policy later if needed.
+            findSocketChannel(s);
+            assertFalse(s.foundChannel());
+        }
+    }
+    
+    @Test
+    public void testNamedNullAndHiddenSocket() throws IOException, ProtocolException {
+        try (HiddenAbstractSocket s = new NamedNullAndHiddenSocket()) {
+            findSocketChannel(s);
+            assertFalse(s.foundChannel());
+        }
+    }
+    
+    private SocketChannel findSocketChannel(Socket s) throws IOException {
+        try {
+	    Method m = Protocol.class.getDeclaredMethod("findSocketChannel", Socket.class);
+	    m.setAccessible(true);
+	    return (SocketChannel) m.invoke((Object) null, s);
+	} catch (RuntimeException re) {
+	    throw re;
+	} catch (Exception e) {
+	    throw new IOException(e);
+	}
+    }
+    
+    private static class RenamedSocketLayer3of3 extends RenamedSocketLayer1of3 {
+    }
+    
+    private static class RenamedSocketLayer2of3 extends RenamedSocketLayer1of3 {
+    }
+    
+    private static class RenamedSocketLayer1of3 extends RenamedAbstractSocket {    
+    }
+    
+    private static abstract class RenamedAbstractSocket extends Socket {
+        private Socket tekcos = new WrappedSocket();
+        
+        public boolean foundChannel() {
+            return WrappedSocket.foundChannel(tekcos);
+        }
+    }
+    
+    private static class NullSocketsRenamedSocket extends RenamedAbstractSocket {
+        @SuppressWarnings("unused") //Reflective access
+	private Socket socket;
+        @SuppressWarnings("unused") //Reflective access
+	private Socket tekcos;
+        
+    }
+    
+    private static class Layer5of5 extends Layer4of5 {
+    }
+    private static class Layer4of5 extends Layer3of5 {
+    }
+    private static class Layer3of5 extends Layer2of5 {
+    }
+    private static class Layer2of5 extends Layer1of5 {
+    }
+    private static class Layer1of5 extends LayerAbstractSocket {
+    }
+    
+    private static abstract class LayerAbstractSocket extends Socket {
+        private final Socket socket = new WrappedSocket();
+        
+        public boolean foundChannel() {
+            return WrappedSocket.foundChannel(socket);
+        }
+    }
+    
+    
+    private static class HiddenSocket2of2 extends HiddenSocket1of2 {
+    }
+    
+    private static class HiddenSocket1of2 extends HiddenAbstractSocket {
+    }
+    
+    private static abstract class HiddenAbstractSocket extends Socket {
+        private final Object hidden = new WrappedSocket();
+        
+        public boolean foundChannel() {
+            return WrappedSocket.foundChannel(hidden);
+        }
+    }
+    
+    private static class NamedNullAndHiddenSocket extends HiddenAbstractSocket {
+    
+        @SuppressWarnings("unused") //Reflective access
+        private Socket socket;
+    }
+    
+    private static class WrappedSocket extends Socket {
+        private boolean found;
+        
+        public static boolean foundChannel(Object ws) {
+            return ws instanceof WrappedSocket
+                  && ((WrappedSocket) ws).found;
+        }
+        
+        @Override
+        public SocketChannel getChannel() {
+            found = true;
+            return null;
+        }
     }
 }
