86506b6ab804e1946e3f96e9740c363ef1e177e1
jmehrens <jason_mehrens@hotmail.com>
2021-03-29 16:42:51 -0500

    Updated changes.txt. Remove redundant store. (cherry picked from commit 6fb386b44a34d6a0b153e0e591f135b029d88895)
    Conflicts:
    	doc/release/CHANGES.txt

--- mail/src/main/java/com/sun/mail/util/logging/LogManagerProperties.java
+++ mail/src/main/java/com/sun/mail/util/logging/LogManagerProperties.java
@@ -127,7 +127,6 @@ final class LogManagerProperties extends Properties {
                 lrgi = null;
                 zisd = null;
                 zdtoi = null;
-                zdtoi = null;
             }
         }
 
