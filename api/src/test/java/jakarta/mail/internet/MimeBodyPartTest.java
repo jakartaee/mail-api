2f898d24cd00a068eed916422063e4147431f852
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-08-27 15:28:16 +0200

    Remove impl test dependency
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- api/src/test/java/jakarta/mail/internet/MimeBodyPartTest.java
+++ api/src/test/java/jakarta/mail/internet/MimeBodyPartTest.java
@@ -19,6 +19,7 @@ package jakarta.mail.internet;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -32,11 +33,11 @@ public class MimeBodyPartTest {
 
     @Test
     public void sessionProperties() throws MessagingException, IOException {
-        try {
+        InputStream input = new ByteArrayInputStream(new byte[0]);
         Properties prop = new Properties();
-        MimeMessage orig = buildFromProperties(prop);
-        MimeMultipart omp = (MimeMultipart) orig.getContent();
-        MimeBodyPart obp = (MimeBodyPart) omp.getBodyPart(0);
+        Session session = Session.getInstance(prop);
+        MimeMessage orig = new MimeMessage(session, input);
+        MimeBodyPart obp = new MimeBodyPart(session, input);
         // No properties added, so default will be checked
         // MimeMessage
         assertTrue(orig.setDefaultTextCharset);
@@ -62,9 +63,11 @@ public class MimeBodyPartTest {
         prop.put("mail.mime.ignoremultipartencoding", Boolean.FALSE);
         prop.put("mail.mime.allowutf8", Boolean.FALSE);
         prop.put("mail.mime.cachemultipart", Boolean.FALSE);
-        orig = buildFromProperties(prop);
-        omp = (MimeMultipart) orig.getContent();
-        obp = (MimeBodyPart) omp.getBodyPart(0);
+        
+        session = Session.getInstance(prop);
+        orig = new MimeMessage(session, input);
+        obp = new MimeBodyPart(session, input);
+
         // MimeMessage
         assertFalse(orig.setDefaultTextCharset);
         assertFalse(orig.setContentTypeFileName);
@@ -81,56 +84,5 @@ public class MimeBodyPartTest {
         assertFalse(obp.ignoreMultipartEncoding);
         assertFalse(obp.allowutf8);
         assertFalse(obp.cacheMultipart);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException(e);
-        }
-    }
-
-    private MimeMessage buildFromProperties(Properties prop) throws MessagingException, IOException {
-        Session s = Session.getInstance(prop);
-        MimeMessage orig = createMessage(s);
-        return orig;
-    }
-
-    private static MimeMessage createMessage(Session s) throws MessagingException {
-        String content = "Mime-Version: 1.0\n" + "Subject: Example\n"
-                + "Content-Type: multipart/mixed; boundary=\"-\"\n" + "\n" + "preamble\n" + "---\n"
-                + "Content-Type: text/x-test\n" + "Content-Transfer-Encoding: quoted-printable\n" + "\n" + "test part\n"
-                + "\n" + "-----\n";
-
-        InputStream input = new AsciiStringInputStream(content);
-        return new MimeMessage(s, input);
-    }
-
-    private static class AsciiStringInputStream extends InputStream {
-
-        private final String input;
-        private int position;
-
-        public AsciiStringInputStream(String input) {
-            this(input, true);
-        }
-
-        public AsciiStringInputStream(String input, boolean strict) {
-            if (strict) {
-                for (int i = 0; i < input.length(); i++) {
-                    if (input.charAt(i) > 0x7F) {
-                        throw new IllegalArgumentException("Not an ASCII string");
-                    }
-                }
-            }
-            this.input = input;
-        }
-
-        @Override
-        public int read() {
-            if (position < input.length()) {
-                return input.charAt(position++) & 0xFF;
-            } else {
-                return -1;
-            }
-        }
-
     }
 }
